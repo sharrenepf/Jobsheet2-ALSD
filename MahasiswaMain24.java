412ed952d6244e33efd163eb89374c655371299f
Sharren <sharrenepf@gmail.com>
2025-02-18 12:35:46 +0700

    percobaan 3

--- MahasiswaMain24.java
+++ MahasiswaMain24.java
@@ -10,6 +10,10 @@ public class MahasiswaMain24 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+        Mahasiswa24 mhs2 = new Mahasiswa24 ("Annisa Nabila", "2141720160", 3.25, "TI 2L"); 
+        mhs2.updateIpk (3.30);
+        mhs2.tampilkanInformasi ();
+
     }
 
 }
